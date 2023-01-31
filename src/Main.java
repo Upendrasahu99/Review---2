812e1ec19d40710c9468b49e6b38f6e3d4c65672
upendra <upendrasahu1199@gmail.com>
2023-01-31 11:23:13 +0530

    Review - 2

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 8;
+        int sum=1;
+        System.out.print( + n + "! = ");
+        for (int i=n; i>=1; i--){
+            sum= sum*i;
+        }
+        System.out.println(sum);
+    }
+}
